c8c607b54a32c7f6ca94a5014c7a36cc7722d4e4
Rajeev Jhaj <32276596+yoyoman0909@users.noreply.github.com>
2020-07-13 00:35:02 +0100

    Master branch

---
+++ distributions_rajeev/README.md
@@ -0,0 +1,9 @@
+#distibutions_rajeev
+My first python package
+
+#Files
+Contains 3 main files, the distribution file, the Gaussian file and the Binomial file.
+
+#Installation
+
+Run "pip install #distibutions_rajeev" in your terminal.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_rajeev',
+      version='0.1',
+      description='Gaussian + Binomial distributions',
+      packages=['distributions_rajeev'],
+      author='Rajeev Jhaj',
+      author_email='me@rajeevj.co.uk',
+      zip_safe=False)
